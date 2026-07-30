b8c7c460b42174c57919781b5da127c4cfc86e3c
ammara-lohani <ammaralohani@gmail.com>
2026-07-30 13:51:13 +0500

    Update Vite config for GitHub Actions deploymentwor

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
+    tailwindcss(),
   ],
-   base: '/ammaralohani/',
+  base: process.env.GITHUB_ACTIONS ? '/ammaralohani/' : '/',
 })
